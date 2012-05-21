4a6e24b2b6325184c15dcb47bbb6480c224ef41b
Joshua Clayton <jclayton@thoughtbot.com>
2012-05-21 09:51:45 -0400

    rails new todos --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = '6d86b8f74040dbfdc28d8ae03f7d7b611ec81db8cd0511b53a1496002706475caf8adf3173bb8e2cf838f7e87c34256240bc84f63814056ca230d65f30d2ce16'
