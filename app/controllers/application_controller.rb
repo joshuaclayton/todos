fbabf071607e56caeae65ab66787bfb1bc181934
Joshua Clayton <jclayton@thoughtbot.com>
2012-07-17 08:13:31 -0400

    Scoping

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,6 @@ class ApplicationController < ActionController::Base
     session[:email] = email_address
   end
 
-  def current_email
-    session[:email]
-  end
-
   def current_user
     if current_email
       User.new(current_email)
@@ -18,9 +14,15 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
+  private
+
   def authenticate
     if !current_user.signed_in?
       redirect_to new_session_path
     end
+  end
+
+  def current_email
+    session[:email]
   end
 end
