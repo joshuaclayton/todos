2f8a8df6a3e02830a8ea551c20997d2a730fe4a2
Joshua Clayton <jclayton@thoughtbot.com>
2012-07-17 08:35:08 -0400

    Set up spork with rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,20 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-require 'rspec/autorun'
+require 'rubygems'
+require 'spork'
+#uncomment the following line to use spork with the debugger
+#require 'spork/ext/ruby-debug'
 
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Spork.prefork do
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
 
-RSpec.configure do |config|
-  config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+  RSpec.configure do |config|
+    config.infer_base_class_for_anonymous_controllers = false
+    config.order = "random"
+  end
+end
+
+Spork.each_run do
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 end
