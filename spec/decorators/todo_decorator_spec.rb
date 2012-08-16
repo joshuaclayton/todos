33f6f149297a9f4cf829b5b89d38b690da0a6589
Joshua Clayton <jclayton@thoughtbot.com>
2012-08-16 08:20:01 -0400

    Organize tests some more

--- spec/decorators/todo_decorator_spec.rb
+++ spec/decorators/todo_decorator_spec.rb
@@ -3,33 +3,37 @@ require 'spec_helper'
 describe TodoDecorator do
   include Rails.application.routes.url_helpers
 
-  it 'returns a link to the completion path if incomplete' do
-    todo = build_stubbed(:todo)
-    result = TodoDecorator.new(todo).completion_link
-    Capybara::Node::Simple.new(result).should have_css("a[data-method='post'][href='#{todo_completion_path(todo)}']", text: 'Complete')
-  end
-
-  it 'returns a link to the completion path if complete' do
-    todo = build_stubbed(:todo, :completed)
-    result = TodoDecorator.new(todo).completion_link
-    Capybara::Node::Simple.new(result).should have_css("a[data-method='delete'][href='#{todo_completion_path(todo)}']", text: 'Incomplete')
-  end
-
-  it 'renders list item for complete todo' do
-    todo = build_stubbed(:todo, :completed)
-    result = TodoDecorator.new(todo).list_item do
-      'string'
+  context 'completion_link' do
+    it 'generates a link to complete the todo when incomplete' do
+      todo = build_stubbed(:todo)
+      result = TodoDecorator.new(todo).completion_link
+      Capybara.string(result).should have_css("a[data-method='post'][href='#{todo_completion_path(todo)}']", text: 'Complete')
     end
 
-    Capybara::Node::Simple.new(result).should have_css("li.complete#todo_#{todo.id}:contains('string')")
+    it 'generates a link to mark the todo as incomplete when complete' do
+      todo = build_stubbed(:todo, :completed)
+      result = TodoDecorator.new(todo).completion_link
+      Capybara.string(result).should have_css("a[data-method='delete'][href='#{todo_completion_path(todo)}']", text: 'Incomplete')
+    end
   end
 
-  it 'renders list item for incomplete todo' do
-    todo = build_stubbed(:todo)
-    result = TodoDecorator.new(todo).list_item do
-      'string'
+  context 'list_item' do
+    it 'renders list item for complete todo' do
+      todo = build_stubbed(:todo, :completed)
+      result = TodoDecorator.new(todo).list_item do
+        'string'
+      end
+
+      Capybara.string(result).should have_css("li#todo_#{todo.id}.complete", text: 'string')
     end
 
-    Capybara::Node::Simple.new(result).should have_css("li#todo_#{todo.id}:contains('string'):not(.complete)")
+    it 'renders list item for incomplete todo' do
+      todo = build_stubbed(:todo)
+      result = TodoDecorator.new(todo).list_item do
+        'string'
+      end
+
+      Capybara.string(result).should have_css("li#todo_#{todo.id}:not(.complete)", text: 'string')
+    end
   end
 end
