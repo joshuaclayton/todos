ad02dfea66d714219a6c9c982cc350823f2656d1
Joshua Clayton <jclayton@thoughtbot.com>
2012-11-12 11:36:22 -0500

    Refactor to move session interaction into one file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def current_email
+    session[:current_email]
+  end
+  helper_method :current_email
+
+  private
+
+  def sign_in_as(email)
+    session[:current_email] = email
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    session[:current_email] = params[:session][:email]
+    sign_in_as params[:session][:email]
     redirect_to todos_path
   end
 end
