62b7d1d0ce4ad6377ea3ab0ef87f897045165add
Joshua Clayton <jclayton@thoughtbot.com>
2013-08-19 12:23:25 -0400

    Refactor spec

--- spec/features/guest_creates_todo_spec.rb
+++ spec/features/guest_creates_todo_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 feature 'Guest creates todo' do
   scenario 'creates a new todo' do
+    create_todo_named 'Buy milk'
+
+    expect(page).to display_todo_named('Buy milk')
+  end
+
+  def create_todo_named(name)
     visit root_path
     click_on 'Add new todo'
-    fill_in 'Name', with: 'Buy milk'
+    fill_in 'Name', with: name
     click_on 'Submit'
+  end
 
-    expect(page).to have_css('.todos li', text: 'Buy milk')
+  def display_todo_named(name)
+    have_css '.todos li', text: name
   end
 end
